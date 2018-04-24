a25b8df4ea65c7c5db7ad334de6e36f3c68145e2
drewmolleur <drewmolleur@gmail.com>
2018-04-24 03:35:01 -0500

    Cleanup
    Sockets

--- src/model/MyClientSocket.java
+++ src/model/MyClientSocket.java
@@ -7,10 +7,14 @@ import java.net.Socket;
 import java.net.UnknownHostException;
 
 public class MyClientSocket {
+    
     public MyClientSocket() {
+        
         (new Thread() {
+            
             @Override
             public void run() {
+
                 //Creates Buffered Writer Client
                 try {
                     Socket s = new Socket( "98.162.212.185" , 52018);
@@ -20,7 +24,6 @@ public class MyClientSocket {
                                             ));
                     //Infinite loop
                     while (true) {
-                        
                         //Supposed to send our move
                         out.write("0|1|-1");
                         System.out.println("0|1|-1");
@@ -30,23 +33,7 @@ public class MyClientSocket {
                 } catch (UnknownHostException e) {
                 } catch (IOException | InterruptedException e) {
                 }
-            }
-        }).start();
+            }   //Starts something or the other...
+        }       ).start();
     }
-/*    private final Socket socket;
-    private final Scanner scanner;
-    private final String serverIP = "98.162.212.185";
-    public MyClientSocket( InetAddress serverAddress, int serverPort ) throws Exception {
-        this.socket = new Socket( serverAddress, serverPort );
-        this.scanner = new Scanner( System.in );
-    }
-    public void start() throws IOException {
-        String input;
-        while ( true ) {
-            input = scanner.nextLine();
-            PrintWriter out = new PrintWriter( this.socket.getOutputStream(), true );
-            out.println( input );
-            out.flush();
-        }
-   }*/
 }
